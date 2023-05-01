853f18b191ca78bc18d4ba49e584cc26b9acab0c
Lucas Avila <lucaxx@gmail.com>
2023-05-01 16:36:39 -0300

    wip

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,4 @@
+type EmptyObject = {};
 /**
  * Left side of the `Either` type.
  */
@@ -34,27 +35,40 @@ type TugError = unknown;
 /**
  * A reader function that reads R, and returns a task that returns Either<unknown, A>.
  */
-export type TugRte<R extends object, A> = Reader<R, Task<Either<TugError, A>>>;
+export type TugRte<R extends EmptyObject, A> = Reader<
+  R,
+  Task<Either<TugError, A>>
+>;
 
 /**
  * A reader function that reads R, and returns a Promise of Either<unknown, A>.
  * This is the core type of this library.
  */
-export type TugRpe<R extends object, A> = Reader<
+export type TugRpe<R extends EmptyObject, A> = Reader<
   R,
   Promise<Either<TugError, A>>
 >;
 
+export type Dependency<R> = {
+  read: (it: any) => R;
+};
+export const Dependency = <R>(): Dependency<R> => ({
+  read: (it) => it,
+});
+
 /**
  * Adds the `use` function to the context.
  */
-export type CreateContext<R extends object> = R & {
+export type CreateContext<R extends EmptyObject> = {
   /**
    * Transforms a `tug` into a plain promise.
    * Returns a successful promise if the `tug` succeeds,
    * or a rejected promise if the `tug` fails.
    */
-  use: <T>(it: tug<R, T>) => Promise<T>;
+  use: <T>(
+    it: [R] extends [never] ? tug<R, T> : R extends R ? tug<R, T> : never
+  ) => Promise<T>;
+  read: <R2>(tag: Dependency<R2>) => R extends R2 ? R2 : never;
 };
 
 /**
@@ -64,7 +78,7 @@ export type CreateContext<R extends object> = R & {
  * If the callback throws an error or returns a rejected promise, the `tug` value will be an error.
  * If the callback returns a value or promise of a value, the `tug` value will be of that value.
  */
-export type TugCallback<R extends object, A> = (
+export type TugCallback<R extends EmptyObject, A> = (
   ctx: CreateContext<R>
 ) => Promise<A> | A;
 
@@ -77,7 +91,7 @@ const unwrapEither = <E, A>(e: Either<E, A>): A => {
 };
 
 const chainRpe =
-  <R extends object, A, B>(
+  <R extends EmptyObject, A, B>(
     rte: TugRpe<R, A>,
     f: (a: A) => TugRpe<R, B>
   ): TugRpe<R, B> =>
@@ -93,25 +107,37 @@ const chainRpe =
 /**
  * The `tug` instance.
  */
-export class tug<R extends object, A> {
+export class tug<R extends EmptyObject, A> {
   private rpe: TugRpe<R, A>;
-  constructor(rte: TugRpe<R, A>) {
+  private dependencies: any;
+  constructor(rte: TugRpe<R, A>, dependencies: any) {
     this.rpe = rte;
+    this.dependencies = dependencies;
   }
 
   /**
    * Creates a ReaderTaskEither from a `tug` instance.
    */
   get rte(): TugRte<R, A> {
-    return (deps: R) => async () => this.rpe(deps);
+    return (dependencies: R) => async () => this.rpe(dependencies);
   }
 
   /**
    * Executes the `tug` instance and returns a promise of the result.
    * If the `tug` fails, the promise will be rejected.
    */
-  exec(deps: R): Promise<A> {
-    return this.execEither(deps).then(unwrapEither);
+  exec: [R] extends [never] ? () => Promise<A> : never = (() => {
+    return this.execEither().then(unwrapEither);
+  }) as any;
+
+  provide<R2 extends EmptyObject>(
+    tag: Dependency<R2>,
+    it: R2
+  ): tug<Exclude<R, R2>, A> {
+    return new tug(this.rpe, {
+      ...this.dependencies,
+      ...it,
+    });
   }
 
   /**
@@ -119,8 +145,8 @@ export class tug<R extends object, A> {
    * If the `tug` fails, the promise will be resolved with a `Left`.
    * If the `tug` succeeds, the promise will be resolved with a `Right`.
    */
-  execEither(deps: R): Promise<Either<TugError, A>> {
-    return this.rpe(deps);
+  execEither(): Promise<Either<TugError, A>> {
+    return this.rpe(this.dependencies);
   }
 
   /**
@@ -140,7 +166,8 @@ export class tug<R extends object, A> {
     f: (a: A, ctx: CreateContext<R2>) => B | Promise<B>
   ): tug<R2, B> {
     return new tug(
-      chainRpe(this.rpe, (a) => TugRpe<R2, B>((ctx) => f(a, ctx)))
+      chainRpe(this.rpe, (a) => tug.TugRpe<R2, B>((ctx) => f(a, ctx))),
+      this.dependencies
     );
   }
 
@@ -150,39 +177,52 @@ export class tug<R extends object, A> {
    * If `this` `tug` succeeded, the returned `tug` will be the result of `f` applied to the value of `this`.
    */
   flatMap<B, R2 extends R>(f: (a: A) => tug<R2, B>): tug<R2, B> {
-    return new tug(chainRpe(this.rpe, (a) => f(a).rpe));
+    return new tug(
+      chainRpe(this.rpe, (a) => f(a).rpe),
+      this.dependencies
+    );
   }
 
   /**
    * Alias for `flatMap`.
    */
   chain<B, R2 extends R>(f: (a: A) => tug<R2, B>): tug<R2, B> {
-    return new tug(chainRpe(this.rpe, (a) => f(a).rpe));
+    return new tug(
+      chainRpe(this.rpe, (a) => f(a).rpe),
+      this.dependencies
+    );
   }
+
+  static TugRpe =
+    <R extends EmptyObject, A>(cb: TugCallback<R, A>): TugRpe<R, A> =>
+    async (dependencies: R) => {
+      const context = {
+        ...dependencies,
+        use: <T>(it: tug<any, T>): Promise<T> =>
+          it
+            .rpe({
+              ...it.dependencies,
+              ...dependencies,
+            })
+            .then(unwrapEither),
+        read: () => dependencies,
+      };
+
+      try {
+        return await Promise.resolve(cb(context as any)).then((right) => ({
+          right,
+          _tag: "Right",
+        }));
+      } catch (left) {
+        return { _tag: "Left", left };
+      }
+    };
 }
 
-const TugRpe =
-  <R extends object, A>(cb: TugCallback<R, A>): TugRpe<R, A> =>
-  async (ctx: R) => {
-    const newCtx = {
-      ...ctx,
-      use: <T>(it: tug<any, T>): Promise<T> => it.exec(newCtx),
-    };
+const newTug = <R extends EmptyObject, A>(cb: TugCallback<R, A>): tug<R, A> =>
+  new tug(tug.TugRpe(cb), {});
 
-    try {
-      return await Promise.resolve(cb(newCtx)).then((right) => ({
-        right,
-        _tag: "Right",
-      }));
-    } catch (left) {
-      return { _tag: "Left", left };
-    }
-  };
-
-const newTug = <R extends object, A>(cb: TugCallback<R, A>): tug<R, A> =>
-  new tug(TugRpe(cb));
-
-type TugBuilder<R extends object> = {
+type TugBuilder<R0 extends EmptyObject> = {
   /**
    * Constructs a new `tug` instance. The callback is executed when the `tug` is executed.
    *
@@ -191,48 +231,50 @@ type TugBuilder<R extends object> = {
    *
    * The callback is passed a context object, which contains the dependencies of the `tug`, and the `use` function.
    */
-  <A>(cb: TugCallback<R, A>): tug<R, A>;
+  <A>(cb: TugCallback<R0, A>): tug<R0, A>;
   /**
    * Constructs a new `tug` instance, with the given value as the result.
    */
-  of: <A>(it: A) => tug<R, A>;
+  of: <A>(it: A) => tug<R0, A>;
   /**
    * Constructs a new `tug` instance, with the given value as the result.
    */
-  right: <A>(it: A) => tug<R, A>;
+  right: <A>(it: A) => tug<R0, A>;
   /**
    * Constructs a new `tug` instance, with the given value as the error.
    */
-  left: <A>(it: any) => tug<R, A>;
-  /**
-   * Constructs a `tug` instance from a Reader-Task-Either.
-   */
-  fromRte: <A>(rte: TugRte<R, A>) => tug<R, A>;
+  left: <A>(it: any) => tug<R0, A>;
+  // /**
+  //  * Constructs a `tug` instance from a Reader-Task-Either.
+  //  */
+  // fromRte: <R extends EmptyObject, A>(rte: TugRte<R, A>) => tug<R & R0, A>;
+
+  depends: <R extends EmptyObject>(it: Dependency<R>) => TugBuilder<R | R0>;
 };
 
-export const tugBuilders = <R extends object>() => {
-  /**
-   * Constructs a new `tug` instance.
-   */
-  const Tug: TugBuilder<R> = new Proxy(newTug, {
-    get: (_target, prop, _receiver) => {
-      if (prop == "of" || prop == "right") {
-        return <T>(it: T) => newTug(() => it);
-      }
-      if (prop == "left") {
-        return <T>(it: T) =>
-          newTug(() => {
-            throw it;
-          });
-      }
-      if (prop === "fromRte") {
-        return <T>(it: TugRte<R, T>) => new tug((d: R) => it(d)());
-      }
-      throw new Error(
-        'Tug does not have a property named "' + String(prop) + '"'
-      );
-    },
-  }) as any;
-
-  return { Tug };
-};
+/**
+ * Constructs a new `tug` instance.
+ */
+export const Tug: TugBuilder<never> = new Proxy(newTug, {
+  get: (_target, prop, _receiver) => {
+    if (prop == "of" || prop == "right") {
+      return <T>(it: T) => newTug(() => it);
+    }
+    if (prop == "left") {
+      return <T>(it: T) =>
+        newTug(() => {
+          throw it;
+        });
+    }
+    if (prop === "fromRte") {
+      return <R extends EmptyObject, T>(it: TugRte<R, T>): tug<R, T> =>
+        new tug((d: R) => it(d)(), {});
+    }
+    if (prop === "depends") {
+      return () => Tug;
+    }
+    throw new Error(
+      'Tug does not have a property named "' + String(prop) + '"'
+    );
+  },
+}) as any;
--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,27 +1,24 @@
 import { expect, test } from "@jest/globals";
-import { tugBuilders } from "../src/core";
-
-type TestCtx = {};
-export const { Tug } = tugBuilders<TestCtx>();
+import { CreateContext, Dependency, Tug } from "../src/core";
 
 test("resolves", async () => {
-  const v1 = await Tug(() => 1).exec({});
+  const v1 = await Tug(() => 1).exec();
   expect(v1).toBe(1);
 
-  const v2 = await Tug(async () => 2).exec({});
+  const v2 = await Tug(async () => 2).exec();
   expect(v2).toBe(2);
 
-  const v3 = await Tug.of(3).exec({});
+  const v3 = await Tug.of(3).exec();
   expect(v3).toBe(3);
 
-  const v4 = await Tug.right(4).exec({});
+  const v4 = await Tug.right(4).exec();
   expect(v4).toBe(4);
 });
 
 test("catches", async () => {
   const v1 = await Tug(() => {
     throw new Error("...");
-  }).execEither({});
+  }).execEither();
   expect(v1).toMatchInlineSnapshot(`
 {
   "_tag": "Left",
@@ -31,7 +28,7 @@ test("catches", async () => {
 
   const v2 = await Tug(() => {
     return Promise.reject(new Error("..."));
-  }).execEither({});
+  }).execEither();
   expect(v2).toMatchInlineSnapshot(`
 {
   "_tag": "Left",
@@ -39,7 +36,7 @@ test("catches", async () => {
 }
 `);
 
-  const v3 = await Tug.left(new Error("...")).execEither({});
+  const v3 = await Tug.left(new Error("...")).execEither();
   expect(v3).toMatchInlineSnapshot(`
 {
   "_tag": "Left",
@@ -52,20 +49,20 @@ test("exec", async () => {
   try {
     await Tug(() => {
       throw new Error("...");
-    }).exec({});
+    }).exec();
     expect(1).toBe(2);
   } catch (e) {
     expect(e).toMatchInlineSnapshot(`[Error: ...]`);
   }
 
-  const v2 = await Tug(async () => 2).exec({});
+  const v2 = await Tug(async () => 2).exec();
   expect(v2).toBe(2);
 });
 
 test("execEither", async () => {
   const v1 = await Tug(() => {
     throw new Error("...");
-  }).execEither({});
+  }).execEither();
 
   expect(v1).toMatchInlineSnapshot(`
 {
@@ -74,21 +71,11 @@ test("execEither", async () => {
 }
 `);
 
-  const v2 = await Tug(async () => 2).execEither({});
+  const v2 = await Tug(async () => 2).execEither();
   expect(v2).toMatchInlineSnapshot(`
 {
   "_tag": "Right",
   "right": 2,
-}
-`);
-});
-
-test("as rte", async () => {
-  const v1 = await Tug(() => 1).rte({})();
-  expect(v1).toMatchInlineSnapshot(`
-{
-  "_tag": "Right",
-  "right": 1,
 }
 `);
 });
@@ -103,16 +90,94 @@ test("use", async () => {
     return a + b;
   });
 
-  expect(await x.exec({})).toBe(3);
+  expect(await x.exec()).toBe(3);
 });
 
 test("same ctx", async () => {
   type D = {
     count: number;
   };
-  const { Tug } = tugBuilders<D>();
-  const y = Tug((d) => d.count * 1);
-  const z = Tug(async (d) => d.count * 2);
+  const DDep = Dependency<D>();
+
+  const y = Tug.depends(DDep)((ctx) => ctx.read(DDep).count * 1);
+
+  const z = Tug.depends(DDep)(async (ctx) => ctx.read(DDep).count * 2);
+
+  const x = Tug.depends(DDep)(async (ctx) => {
+    const a = await ctx.use(y);
+    const b = await ctx.use(z);
+    return a + b;
+  });
+
+  if (1 < 0) {
+    //@ts-expect-error
+    x.exec();
+  }
+
+  expect(
+    await x
+      .provide(DDep, {
+        count: 1,
+      })
+      .exec()
+  ).toBe(3);
+});
+
+test("different ctx", async () => {
+  type D1 = {
+    count1: number;
+  };
+  const D1Dep = Dependency<D1>();
+
+  type D2 = {
+    count2: number;
+  };
+  const D2Dep = Dependency<D2>();
+
+  const y = Tug.depends(D1Dep)((ctx) => ctx.read(D1Dep).count1 * 1);
+
+  const z = Tug.depends(D2Dep)(async (ctx) => ctx.read(D2Dep).count2 * 2);
+
+  const x = Tug.depends(D1Dep).depends(D2Dep)(async (ctx) => {
+    const a = await ctx.use(y);
+    const b = await ctx.use(z);
+    return a + b;
+  });
+
+  expect(
+    await x
+      .provide(D1Dep, {
+        count1: 1,
+      })
+      .provide(D2Dep, {
+        count2: 2,
+      })
+      .exec()
+  ).toBe(5);
+});
+
+test("different ctx + provide", async () => {
+  type D1 = {
+    count1: number;
+  };
+  const D1Dep = Dependency<D1>();
+
+  type D2 = {
+    count2: number;
+  };
+  const D2Dep = Dependency<D2>();
+
+  const y = Tug.depends(D1Dep)((ctx) => {
+    return ctx.read(D1Dep).count1 * 1;
+  }).provide(D1Dep, {
+    count1: 1,
+  });
+
+  const z = Tug.depends(D2Dep)(
+    async (ctx) => ctx.read(D2Dep).count2 * 2
+  ).provide(D2Dep, {
+    count2: 2,
+  });
 
   const x = Tug(async (ctx) => {
     const a = await ctx.use(y);
@@ -120,53 +185,20 @@ test("same ctx", async () => {
     return a + b;
   });
 
-  expect(
-    await x.exec({
-      count: 3,
-    })
-  ).toBe(9);
-});
-
-test("different ctx", async () => {
-  type D1 = {
-    count1: number;
-  };
-  const { Tug: Tug1 } = tugBuilders<D1>();
-  type D2 = {
-    count2: number;
-  };
-  const { Tug: Tug2 } = tugBuilders<D2>();
-
-  const y = Tug1((d: D1) => d.count1 * 1);
-  const z = Tug2(async (d) => d.count2 * 2);
-
-  const { Tug: Tug3 } = tugBuilders<D1 & D2>();
-
-  const x = Tug3(async (ctx) => {
-    const a = await ctx.use(y);
-    const b = await ctx.use(z);
-    return a + b;
-  });
-
-  expect(
-    await x.exec({
-      count1: 1,
-      count2: 2,
-    })
-  ).toBe(5);
+  expect(await x.exec()).toBe(5);
 });
 
 test("map", async () => {
   const v1 = await Tug(() => 1)
     .map((it) => it + 1)
-    .exec({});
+    .exec();
   expect(v1).toBe(2);
 });
 
 test("flatMap", async () => {
   const v1 = await Tug(() => 1)
     .flatMap((it) => Tug.of(String(it)))
-    .exec({});
+    .exec();
   expect(v1).toBe("1");
 });
 
@@ -174,22 +206,32 @@ test("tug transform", async () => {
   const v0 = Tug.of(3);
   const v1 = await Tug(() => 1)
     .tug(async (it, ctx) => it + (await ctx.use(v0)))
-    .exec({});
+    .exec();
   expect(v1).toBe(4);
 });
 
 test("chain", async () => {
   const v1 = await Tug(() => 1)
     .chain((it) => Tug.of(String(it)))
-    .exec({});
+    .exec();
   expect(v1).toBe("1");
 });
 
-test("fromRte", async () => {
-  const rte = (_deps: {}) => async () => ({ _tag: "Right" as const, right: 1 });
-  const v1 = await Tug.fromRte(rte).exec({});
-  expect(v1).toBe(1);
-});
+// test("as rte", async () => {
+//   const v1 = await Tug(() => 1).rte({})();
+//   expect(v1).toMatchInlineSnapshot(`
+// {
+//   "_tag": "Right",
+//   "right": 1,
+// }
+// `);
+// });
+
+// test("fromRte", async () => {
+//   const rte = (_deps: {}) => async () => ({ _tag: "Right" as const, right: 1 });
+//   const v1 = await Tug.fromRte(rte).exec();
+//   expect(v1).toBe(1);
+// });
 
 // TODO: widening
 // import { Tug } from "./core";
